054c94b2af8de374e736fb5105a44b1d7a2c237b
pjmoo <00pjmoooo@gmail.com>
2026-08-20 09:40:52 +0900

    Initial commit

---
+++ src/main/java/org/example/jwtfetch/JwtFetchApplication.java
@@ -0,0 +1,13 @@
+package org.example.jwtfetch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtFetchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtFetchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/jwtfetch/JwtFetchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jwtfetch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtFetchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
